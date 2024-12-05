8ee3f6b82faf091ed18dce0167f886de83a7e358
DormentCoder <29092866@students.lincoln.ac.uk>
2024-12-05 14:06:32 +0000

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,17 @@
+#------------------
+
+# Simple Calculator
+
+#------------------
+
+print("Simple Calculator")
+print()
+
+num1 = int(input("Enter a number between 1 and 10: "))
+num2 = int(input("Enter a number between 10 and 1000:"))
+
+add = num1 + num2
+sub = num2 - num1
+
+print("num1 + num2 = " + str(add))
+print("num2 - num1 = " + str(sub))
